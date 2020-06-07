f96f0c146b9961c24fd7fd1758c4941b09ff7fd3
leu <leuflatworm@gmail.com>
2020-06-08 06:19:37 +0900

    add wifi conecting

--- counter.ino
+++ counter.ino
@@ -1,4 +1,14 @@
-n#include "M5Atom.h"
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+#include "M5Atom.h"
+#include <EEPROM.h>
+
+const char *wifi_ssid = "";
+const char *wifi_pass = "";
+
 
 uint8_t counter = 0; //1の位カウンター
 uint8_t counter10 = 0; //10の位カウンター
@@ -11,10 +21,28 @@ const CRGB COLOR[4] = { //色設定
   0x707070
 }; 
 
-void setup() { 
+void setup() {
     M5.begin(true, false, true);
     delay(50);
     M5.dis.clear();
+
+    WiFi.begin(wifi_ssid, wifi_pass);
+    int w = 0;
+    while (WiFi.status() != WL_CONNECTED ) {
+      M5.dis.drawpix(w, COLOR[0]);
+      delay(1000);
+
+      w++;
+      if (w>24){
+        break;
+      }
+    }
+
+    
+    for (int i=0; i<25; i++) { //中央3列の表示をリセット
+      M5.dis.drawpix(i, 0x000000);
+    }
+
     
     //最初に0を表示する
     M5.dis.drawpix(1, COLOR[2]);
@@ -24,7 +52,7 @@ void setup() {
     M5.dis.drawpix(8, COLOR[2]);
     M5.dis.drawpix(11, COLOR[2]);
     M5.dis.drawpix(13, COLOR[2]);
-    M5.dis.drawpix(16, COLOR[2]); 
+    M5.dis.drawpix(16, COLOR[2]);
     M5.dis.drawpix(18, COLOR[2]);
     M5.dis.drawpix(21, COLOR[2]);
     M5.dis.drawpix(22, COLOR[2]);
@@ -61,123 +89,123 @@ void loop() {
           }
         }
 
-            if(counter == 0){ //0の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(16, COLOR[2]); 
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 1){ //1の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(7, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(17, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 2){ //2の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(16, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 3){ //3の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 4){ //4の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 5){ //5の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 6){ //6の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(16, COLOR[2]); 
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 7){ //7の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 8){ //8の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(16, COLOR[2]); 
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }else if(counter == 9){ //9の表示
-                M5.dis.drawpix(1, COLOR[2]);
-                M5.dis.drawpix(2, COLOR[2]);
-                M5.dis.drawpix(3, COLOR[2]);
-                M5.dis.drawpix(6, COLOR[2]);
-                M5.dis.drawpix(8, COLOR[2]);
-                M5.dis.drawpix(11, COLOR[2]);
-                M5.dis.drawpix(12, COLOR[2]);
-                M5.dis.drawpix(13, COLOR[2]);
-                M5.dis.drawpix(18, COLOR[2]);
-                M5.dis.drawpix(21, COLOR[2]);
-                M5.dis.drawpix(22, COLOR[2]);
-                M5.dis.drawpix(23, COLOR[2]);
-            }
+        if(counter == 0){ //0の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(16, COLOR[2]); 
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 1){ //1の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(7, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(17, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 2){ //2の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(16, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 3){ //3の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 4){ //4の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 5){ //5の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 6){ //6の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(16, COLOR[2]); 
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 7){ //7の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 8){ //8の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(16, COLOR[2]); 
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }else if(counter == 9){ //9の表示
+            M5.dis.drawpix(1, COLOR[2]);
+            M5.dis.drawpix(2, COLOR[2]);
+            M5.dis.drawpix(3, COLOR[2]);
+            M5.dis.drawpix(6, COLOR[2]);
+            M5.dis.drawpix(8, COLOR[2]);
+            M5.dis.drawpix(11, COLOR[2]);
+            M5.dis.drawpix(12, COLOR[2]);
+            M5.dis.drawpix(13, COLOR[2]);
+            M5.dis.drawpix(18, COLOR[2]);
+            M5.dis.drawpix(21, COLOR[2]);
+            M5.dis.drawpix(22, COLOR[2]);
+            M5.dis.drawpix(23, COLOR[2]);
+        }
         M5.update();
         
     }
