2d534e6f344049a202c016f7c64b642a25cd4d77
leuflatworm <leuflatworm@gmail.com>
2020-06-18 02:45:46 +0900

    improving readablity

--- counter.ino
+++ counter.ino
@@ -1,13 +1,7 @@
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFi.h>
-#include <WiFiUdp.h>
+
 
 #include "M5Atom.h"
 #include <EEPROM.h>
-
-const char *wifi_ssid = ""; //wifiの名前
-const char *wifi_pass = ""; //wifiのパスワード
 
 
 uint8_t counter = 0; //1の位カウンター
@@ -21,23 +15,30 @@ const CRGB COLOR[4] = { //色設定
   0x707070
 }; 
 
+int NUM_0[12] = {1,2,3,6,8,11,13,16,18,21,22,23};
+int NUM_1[8] = {1,2,7,12,17,21,22,23};
+int NUM_2[11] = {1,2,3,8,11,12,13,16,21,22,23};
+int NUM_3[11] = {1,2,3,8,11,12,13,18,21,22,23};
+int NUM_4[9] = {1,3,6,8,11,12,13,18,23};
+int NUM_5[11] = {1,2,3,6,11,12,13,18,21,22,23};
+int NUM_6[12] = {1,2,3,6,11,12,13,16,18,21,22,23};
+int NUM_7[7] = {1,2,3,8,13,18,23};
+int NUM_8[13] = {1,2,3,6,8,11,12,13,16,18,21,22,23};
+int NUM_9[12] = {1,2,3,6,8,11,12,13,18,21,22,23};
+
+
+
+
+void LEDDISP(int *points, int points_num){
+  for(int i;i<points_num;i++){
+    M5.dis.drawpix(points[i], COLOR[2]);
+  }
+}
+
 void setup() {
     M5.begin(true, false, true);
     delay(50);
     M5.dis.clear();
-
-    WiFi.begin(wifi_ssid, wifi_pass);
-    int w = 0;
-    while (WiFi.status() != WL_CONNECTED ) {
-      M5.dis.drawpix(w, COLOR[0]);
-      delay(1000);
-
-      w++;
-      if (w>24){
-        break;
-      }
-    }
-
     
     for (int i=0; i<25; i++) { //中央3列の表示をリセット
       M5.dis.drawpix(i, 0x000000);
@@ -45,19 +46,9 @@ void setup() {
 
     
     //最初に0を表示する
-    M5.dis.drawpix(1, COLOR[2]);
-    M5.dis.drawpix(2, COLOR[2]);
-    M5.dis.drawpix(3, COLOR[2]);
-    M5.dis.drawpix(6, COLOR[2]);
-    M5.dis.drawpix(8, COLOR[2]);
-    M5.dis.drawpix(11, COLOR[2]);
-    M5.dis.drawpix(13, COLOR[2]);
-    M5.dis.drawpix(16, COLOR[2]);
-    M5.dis.drawpix(18, COLOR[2]);
-    M5.dis.drawpix(21, COLOR[2]);
-    M5.dis.drawpix(22, COLOR[2]);
-    M5.dis.drawpix(23, COLOR[2]);
+    LEDDISP(NUM_0, 12);
     M5.update();
+    
     
 }
 
@@ -90,126 +81,30 @@ void loop() {
         }
 
         if(counter == 0){ //0の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(16, COLOR[2]); 
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_0, 12);
         }else if(counter == 1){ //1の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(7, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(17, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_1, 8);
         }else if(counter == 2){ //2の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(16, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_2, 11);
         }else if(counter == 3){ //3の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_3, 11);
         }else if(counter == 4){ //4の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_4, 9);
         }else if(counter == 5){ //5の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_5, 11);
         }else if(counter == 6){ //6の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(16, COLOR[2]); 
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_6, 12);
         }else if(counter == 7){ //7の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_7, 7);
         }else if(counter == 8){ //8の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(16, COLOR[2]); 
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_8, 13);
         }else if(counter == 9){ //9の表示
-            M5.dis.drawpix(1, COLOR[2]);
-            M5.dis.drawpix(2, COLOR[2]);
-            M5.dis.drawpix(3, COLOR[2]);
-            M5.dis.drawpix(6, COLOR[2]);
-            M5.dis.drawpix(8, COLOR[2]);
-            M5.dis.drawpix(11, COLOR[2]);
-            M5.dis.drawpix(12, COLOR[2]);
-            M5.dis.drawpix(13, COLOR[2]);
-            M5.dis.drawpix(18, COLOR[2]);
-            M5.dis.drawpix(21, COLOR[2]);
-            M5.dis.drawpix(22, COLOR[2]);
-            M5.dis.drawpix(23, COLOR[2]);
+          LEDDISP(NUM_9, 12);
         }
         M5.update();
         
     }
     
-    delay(10); //ディレイ時間を長くしすぎると素早く入力した時に認識されない
+    delay(50); //ディレイ時間を長くしすぎると素早く入力した時に認識されない
     M5.update();
 }
